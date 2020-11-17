73d499e09a86a3f85f6ce4c7410b925d6d29867d
abdus <azad3652@gmail.com>
2020-11-17 16:32:48 +0000

    Deploying to gh-pages from  @ cdbd5fabbc72a5e07a39a86ce3b6f83c2e951a1e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.319687ad6045fe6948771f036b833adb.js"
+  "./precache-manifest.3890d208ba0a6130b6efd129f88c4a35.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
